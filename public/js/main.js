48ebeb28d2288e3bc36c5bc3cbb5c0e1aca838b7
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-03 11:36:53 +0100

    exo2

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,21 @@
 //EXO1
-function reverseNumber(nombre) {
-    return nombre.toString().split('').reverse().join('')
+// function reverseNumber(nombre) {
+//     return nombre.toString().split('').reverse().join('')
     
+// }
+
+// console.log(reverseNumber(583));
+
+//EXO2
+function divPar2(nombre) {
+    switch (nombre % 2) {
+        case 0:
+        return `Le numéro ${nombre} est divisible par 2 : ${nombre} / 2 = ${nombre / 2}`;
+
+        default:
+        return `Ce nombre n'est pas divisible par 2`;
+    }
 }
 
-console.log(reverseNumber(583));
+console.log(divPar2(14));
+console.log(divPar2(15));
